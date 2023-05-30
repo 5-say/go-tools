0eb7ec2ec08d1b9fbcfdab153ca97e634634a7da
b <b@b.b>
2023-05-30 22:31:21 +0800

    update

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# go-tools
+
+与业务无关的 golang 工具补充
+
+> 有任何需求，请优先考虑 [lo 工具集](https://github.com/samber/lo) 避免重复造轮子
--- t/pointer.go
+++ t/pointer.go
@@ -1,5 +1,6 @@
 package t
 
+// Pointer .. 指针取值
 func Pointer[T any](input *T, defaultVal T) T {
 	if input != nil {
 		return *input
